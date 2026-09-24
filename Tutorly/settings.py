4014d49a3ae142868b4109272ba3c0fa61190bad
Aasath <aasatheng2003@gmail.com>
2026-09-24 20:06:16 +0530

    Student model changed and some serializer Changes

--- Tutorly/settings.py
+++ Tutorly/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'grade',
     'subject',
     'teacher',
@@ -47,6 +48,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -132,3 +135,7 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
--- student/models.py
+++ student/models.py
@@ -2,28 +2,32 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
-
 def get_default_dob():
     return timezone.now().date() - datetime.timedelta(days=365 * 10)
 
-
 class Student(models.Model):
+    class Gender(models.TextChoices):
+        BOY = 'Boy', 'Boy'
+        GIRL = 'Girl', 'Girl'
+        
     student_id = models.CharField(max_length=10, unique=True)
     name = models.CharField(max_length=128)
-    dob = models.DateField(
-        null=True, blank=True
-    )  # illana default=get_default_dob
+    gender = models.CharField(max_length=4, choices=Gender.choices , default = 'BOY')
+    
+    # Neenga yositha maari default function-ai inga use pannalam
+    dob = models.DateField(null=True, blank=True, default=get_default_dob)
+    
     parent_name = models.CharField(max_length=128)
-    parent_phone = models.CharField(
-        max_length=12
-    )  # Siblings-kaaga unique thevai illa
-    whatsapp_number = models.CharField(max_length=12, blank=True)
+    # length 15 irunthaal "+94 77 123 4567" endru spaces irunthalum problem illai
+    parent_phone = models.CharField(max_length=15) 
+    whatsapp_number = models.CharField(max_length=15, blank=True)
+    
     is_active = models.BooleanField(default=True)
     registered_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def save(self, *args, **kwargs):
-        # WhatsAPP number illai enraal parent_phone-ah default-aa vaikkom
+        # whatsapp_number empty string ("") aaga irunthalum intha condition true aagum
         if not self.whatsapp_number:
             self.whatsapp_number = self.parent_phone
         super().save(*args, **kwargs)
